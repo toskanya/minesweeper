6e6f25c677707bd6dfb96e17425442357760b8e1
toskanya <trandanghienlong2004@gmail.com>
2023-09-23 15:06:18 +0700

    add toggle function for buttons

--- utilities.py
+++ utilities.py
@@ -8,3 +8,12 @@ def width_prct(percentage):
 
 def get_widget_size(widget):
     return widget.winfo_width(), widget.winfo_height()
+
+is_on = False
+def toggle(minesweeperAI):
+    global is_on
+    is_on = not is_on
+    if is_on:
+        minesweeperAI.start()
+    else:
+        minesweeperAI.stop()
